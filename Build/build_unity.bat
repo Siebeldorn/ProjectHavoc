bdd793f83b017dfd2de884f3fad723953afc7774
Roland <Siebeldorn@users.noreply.github.com>
2020-05-23 21:01:21 +0200

    added batch script to generate Unity build from commandline
    refs #6

---
+++ Build/build_unity.bat
@@ -0,0 +1,25 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+ECHO searching registry for Unity installation path
+REG QUERY "HKCU\Software\Unity Technologies\Installer\Unity" /v "Location x64" >nul 2>&1
+
+IF %ERRORLEVEL% == 0 (
+    FOR /f "tokens=3*" %%a in ('REG QUERY "HKCU\Software\Unity Technologies\Installer\Unity" /v "Location x64"') DO SET unity=%%b
+    ECHO Unity path: "!unity!"
+    
+    SET currpath=%~dp0
+    SET projectpath="!currpath!..\Unity"
+    SET logpath=!currpath!unity.log
+    SET outputpath=!currpath!output\unity\ProjectHavoc.exe
+    
+    echo starting build for Unity project at !projectpath!...
+    "!unity!\Editor\Unity.exe" -quit -batchmode -projectPath !projectpath! -logFile !logpath! -buildWindows64Player !outputpath!
+    IF !ERRORLEVEL! == 0 (
+        echo build successful!
+    )
+) ELSE (
+    ECHO ERROR: Unity not found!
+)
+
+pause
