b201b763fd3995f315dbc0396899935b479f34fe
JanAppold <contact@janappold.com>
2020-06-01 19:11:25 +0200

    Addet Basic Turret with animation
    Added Turret mesh without textures for now
    Added animation controller script to play animation ingame
    To play the animation hit play and ingame hit "Q", it will loop for now.
    To turn this off click on the Mesh itself in the project explorer. In the inspector you will see a "loop button"
    
    Mesh has a "root" for the turret itself for ez rotation transformation.

---
+++ Unity/Assets/Scripts/TurretAnimationController.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class TurretAnimationController : MonoBehaviour
+{
+
+    public Animator turretAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turretAnim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if(Input.GetKeyDown("q"))
+        {
+            turretAnim.Play("CINEMA_4D_Main");
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            turretAnim.Play("Default");
+        }
+    }
+}
