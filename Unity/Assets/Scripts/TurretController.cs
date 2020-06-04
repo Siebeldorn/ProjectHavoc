6a918f27371f5cba85597ef257ecddb32065b9ca
Roland <Siebeldorn@users.noreply.github.com>
2020-06-04 19:13:09 +0200

    created simple TurretController and added this and the AnimationController to turret prefab

---
+++ Unity/Assets/Scripts/TurretController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        Debug.Assert(animator != null, "Cannot find Animator component for TurretController");
+    }
+
+    void Update()
+    {
+        if ( animator != null )
+        {
+            if ( animator.GetCurrentAnimatorStateInfo(0).IsName("idle") )
+            {
+                animator.Play("shoot");
+            }
+        }
+    }
+}
